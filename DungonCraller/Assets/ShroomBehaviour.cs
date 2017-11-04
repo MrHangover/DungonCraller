02ff1da5323b7adfc2c59551fa77881815980d4b
Jacob L. Olsen <jacoblynggaardolsen@hotmail.com>
2017-11-04 20:23:52 +0100

    Moved assets

---
+++ DungonCraller/Assets/ShroomBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShroomBehaviour : MonoBehaviour {
+
+    public Animator animator;
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.P)){
+            animator.SetTrigger("Attack");
+        }
+    }
+}
