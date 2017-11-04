8cae042226496684b7131077473fb1e3bd605483
Kasper Ingdahl Andkjær <kasper@flickerlab.com>
2017-11-04 17:37:39 +0100

    Started work on the room-generator. Added level fader.

---
+++ DungonCraller/Assets/Scripts/RoomGeneration/RoomContent/BaseRoomContentSpawnType.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseRoomContentSpawnType : MonoBehaviour
+{
+    protected List<BaseRoomContentItem> _items = new List<BaseRoomContentItem>();
+    public abstract void Spawn ();
+
+    public void DeleteAll ()
+    {
+        for(int i = _items.Count-1; i >= 0; i--)
+        {
+            DestroyImmediate (_items[i]);
+        }
+
+        _items.Clear ();
+    }
+}
