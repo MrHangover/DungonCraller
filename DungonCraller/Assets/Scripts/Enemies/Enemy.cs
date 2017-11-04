05cd5ab8f371c06a55d4dbe4d198e6bb91edac2c
BJARKESPC\bjark <mail@bjarke.it>
2017-11-04 15:01:43 +0100

    enemy start

---
+++ DungonCraller/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour {
+
+    public float health;
+    public float speed;
+    public Weapon weapon;
+
+	// Use this for initialization
+	public virtual void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public virtual void Update () {
+		
+	}
+
+    public virtual void Attack()
+    {
+
+    }
+
+    public virtual void Die()
+    {
+
+    }
+
+
+
+}
---
+++ DungonCraller/Assets/Scripts/Enemies/EnemySkeleton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySkeleton : Enemy {
+
+	// Use this for initialization
+	public override void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	public override void Update ()
+    {
+		
+	}
+
+    public override void Attack()
+    {
+
+    }
+
+    public override void Die()
+    {
+
+    }
+
+}
